36ca91588fbd0a653a20ac8528f2db33b878671f
AlexVcom <doalvor@gmial.com>
2024-04-04 23:09:53 +0200

    Init landmarks

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -1,17 +1,13 @@
-//
-//  LandmarksApp.swift
-//  Landmarks
-//
-//  Created by Koty on 03/04/2024.
-//
-
 import SwiftUI
 
 @main
 struct LandmarksApp: App {
+    @State private var modelData = ModelData()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(modelData)
         }
     }
 }
